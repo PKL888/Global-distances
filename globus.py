94b65976762754e8af45a3f99904c36ec045f0ca
PKL888 <pelegkl888@gmail.com>
2018-04-17 16:01:36 +1000

    Applied Relative Path and String Formatting

--- globus.py
+++ globus.py
@@ -5,50 +5,54 @@
 import math
 import pandas as pd
 
-# Opening and reading the file
-p = pd.read_csv("simplemaps_worldcities_basic.csv")
+# Functions 
+def read_database(path):
+	pd.read_csv(path)
 
-# Defining the user input
-user_city1 = raw_input("Please enter the name of your first city: ")
-user_country_1 = raw_input("Please enter the name of your first country: ")
-user_city2 = raw_input("Please enter the name of your second city: ")
-user_country_2 = raw_input("Please enter the name of your second country: ")
+def capture_user_input():
+	raw_input("")
 
-# The mathematical calculation of the Great Circle Distance 
-def greatCircleDistance(lat1, lon1, lat2, lon2):
-	r = 6371 # Radius of the Earth in km
+def subset_data(data, city, country):	
+	cityX = data[(data['City'] == city) & (data['Country'] == country)]
+	calculate_latlon(cityX)
 
-	# Variables
+def calculate_latlon(name):
+	lat = name.iloc[0]['Lat']
+	lon = name.iloc[0]['Lon']
+
+def greatCircleDistance(lat1, lat2, lon1, lon2):
+	r = 6371 
+
 	j1 = math.radians(lat1) 
 	j2 = math.radians(lat2)
 	hj = math.radians(lat2-lat1)
 	hi = math.radians(lon2-lon1)
 
-	# Calculation according to the Haversine Formula
 	a = math.sin(hj/2) * math.sin(hj/2) + math.cos(j1) * math.cos(j2) * math.sin(hi/2) * math.sin(hi/2)
-
 	c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-
 	d = r * c
 
-	# Printing out the result
-	print("\nThe distance between %s & %s is %d km" % (user_city1, user_city2, d))
+	print("The distance between %s & %s is %d km" % (user_city1, user_city2, d))
 
-# A new dataframe with a subset of p, and has only the rows appertaining to Barcelona
-cityA = p[(p['City'] == user_city1) & (p['Country'] == user_country_1)]
-cityB = p[(p['City'] == user_city2) & (p['Country'] == user_country_2)]
+def display_output(cityA, countryA, cityB, countryB, distance):
+	print distance
 
-# Getting the latitude and longitude of the first city
-lat1 = cityA.iloc[0]['Lat']
-lon1 = cityA.iloc[0]['Lon']
+	print("%s --> %s" % (cityA, countryA))
+	print("%s --> %s" % (cityB, countryB))
 
-# Getting the latitude and longitude of the second city
-lat2 = cityB.iloc[0]['Lat']
-lon2 = cityB.iloc[0]['Lon']
 
-# Putting everything together
-print greatCircleDistance(lat1, lon1, lat2, lon2)
+# Reading the database
+p = read_database("simplemaps_worldcities_basic.csv")
 
-# Debugging and presenting the result
-print("\n%s --> %s" % (user_city1, user_country_1))
-print("%s --> %s" % (user_city2, user_country_2))
+# Getting the user input [I]
+user_city1, user_country_1, user_city2, user_country2 = capture_user_input()
+
+# Calculating the location of the cities
+lat1, lon1 = calculate_latlon(p, user_city1, user_country1)
+lat2, lon2 = calculate_latlon(p, user_city2, user_country2)
+
+# Calculating the final distance using the Great Circle Distance function
+distance = greatCircleDistance(lat1, lon1, lat2, lon2)
+
+# Displaying the result [O]
+display_output(user_city1, user_country_1, user_city2, user_country2, distance)
